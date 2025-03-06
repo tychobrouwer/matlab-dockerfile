2196cc6952f82673aae174d13dcb91b9ab9413db
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2025-03-06 17:04:30 +0100

    update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ sudo systemctl start docker
 
 #docker build -t matlab_with_toolboxes:2024b .
 
-docker run -it --name matlab -v /home/me/Projects:/home/matlab/Projects --network host --shm-size=512M matlab_with_toolboxes:R2024b -browser || docker start -i matlab
+docker run -it --name matlab -v /home/me/Projects:/home/matlab/Projects --privileged -v /dev/bus/usb:/dev/bus/usb --network host --shm-size=512M matlab_with_toolboxes:R2024b -browser || docker start -i matlab
 
